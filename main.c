bc5658fb603a7a46a05094c344e77e6fe760b259
Ivan TITOV <i.titov@students.fr>
2023-11-19 20:07:48 +0100

    initial commit

--- main.c
+++ main.c
@@ -2,33 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-    unsigned short type = 0;
-    int sum = 0;
-    float dollar = 0.92;
-    float euro = 1.09;
-    printf("Conversion des monnaies:\n");
-    printf("Le prix de dollar est  %.2f\n", dollar);
-    printf("Le prix d'euro est %.2f\n\n", euro);
-    printf("Echanger des dollars contre des eur,choisisez 1: \n");
-    printf("Echanger des euros contre des dollars,choisisez 2: \n");
-
-    printf("\n Choisisez le type d'action:\n");
-    scanf("%d", &type);
-
+    float tax = 0.25;
+    float salaries = 0;
+    float salaries_net;
+    printf("Calcul du salaire net:\n");
     printf("\n Selectionner le montant:\n");
-    scanf("%d", &sum);
+    scanf("%f", &salaries);
 
-    switch (type) {
-        case 1:
-            printf("Votre montant est: %.2f\n",sum * dollar);
-            break;
-        case 2:
-            printf("Votre montant est: %f \n", sum * euro);
-            break;
-        default:
-            printf("Le choix n'est pas correct \n");
-            break;
-    }
+    salaries_net = salaries - (salaries * tax);
+    printf("salaire net: %.2f\n", salaries_net);
+
+
 
     return 0;
 }
